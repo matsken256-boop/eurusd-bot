80e64dfeef2ab0c9b3103404d43f8e0727a45fe5
Matsiko Keneth <matsken256@gmail.com>
2026-09-22 15:10:50 +0300

    Create package.js

---
+++ package.js
@@ -0,0 +1,15 @@
+{
+  "name": "eurusd-bot",
+  "version": "1.0.0",
+  "main": "index.js",
+  "type": "commonjs",
+  "dependencies": {
+    "axios": "^1.6.0",
+    "dotenv": "^16.3.0",
+    "mongoose": "^8.0.0",
+    "node-cron": "^3.0.0"
+  },
+  "scripts": {
+    "start": "node index.js"
+  }
+}
